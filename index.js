b6b943a4bd777d56f6d5aedd32e21b6c0e11965b
traceymmbonelungahi <t.lungahi@gmail.com>
2022-06-16 17:26:07 +0300

    commit tree 511b7293289db4d8a0b0d8a00c2702b3f5ca1315

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let backWord = word.split("").reverse().join("");
+
+  return word === backWord? true : false;
 }
 
 /* 
   Add your pseudocode here
 */
-
+// write a function that receives an argument that is a string
+// use the split method on the string to split the string into an array of substrings
+// reverse the new array
+// use the join method to join the elements of the array to a string
+// compare both the original and reversed string
+// if the strings match, should return true if not, should return false 
 /*
   Add written explanation of your solution here
+  For the string to be reversed, the .split() has to be used so that the string can be turned into a new array. The reverse() method reverses the elements in the array. The join() method then joins those elements into a string. The reversde and original strings are then compared, if they match, the output will be true, if not, the output will be false. 
 */
 
 // You can run `node index.js` to view these console logs
